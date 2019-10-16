e4a9377a0deb5d186a57c3bf263a1bcc90ce4c91
Viet Hoang <cooro1994@gmail.com>
2019-10-17 07:41:29 +0900

    feat: all necessary features for test client

--- src/resources/static/js/main.js
+++ src/resources/static/js/main.js
@@ -2,14 +2,20 @@
 
 var usernamePage = document.querySelector('#username-page');
 var chatPage = document.querySelector('#chat-page');
+var listPage = document.querySelector('#list-page');
 var usernameForm = document.querySelector('#usernameForm');
 var messageForm = document.querySelector('#messageForm');
 var messageInput = document.querySelector('#message');
 var messageArea = document.querySelector('#messageArea');
+var listArea = document.querySelector('#listArea');
 var connectingElement = document.querySelector('.connecting');
+var onlineButton = document.querySelector('#online-button');
+var backButton = document.querySelector('#back-button');
 
 var stompClient = null;
 var username = null;
+
+const DOMAIN = "http://localhost:8080"
 
 var colors = [
     '#2196F3', '#32c787', '#00BCD4', '#ff5652',
@@ -19,11 +25,12 @@ var colors = [
 function connect(event) {
     username = document.querySelector('#name').value.trim();
 
-    if(username) {
+    if (username) {
         usernamePage.classList.add('hidden');
+        listPage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('http://127.0.0.1:8080/ws');
+        var socket = new SockJS(DOMAIN + '/ws');
         stompClient = Stomp.over(socket);
 
         stompClient.connect({}, onConnected, onError);
@@ -32,12 +39,17 @@ function connect(event) {
 }
 
 
-function onConnected() {
+async function onConnected() {
+    // Get recent messages
+    var response = await axios.get(DOMAIN + '/messages');
+    for (var message of response.data) {
+        displayMessage(message);
+    }
     // Subscribe to the Public Topic
     stompClient.subscribe('/topic/group', onMessageReceived);
 
     // Tell your username to the server
-    stompClient.send("/app/chat",
+    stompClient.send("/app/join",
         {},
         JSON.stringify({sender: username, type: 'JOIN'})
     )
@@ -47,14 +59,13 @@ function onConnected() {
 
 
 function onError(error) {
-    connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
+    connectingElement.textContent = 'Could not connect to server. Please refresh this page to try again!';
     connectingElement.style.color = 'red';
 }
 
-
 function sendMessage(event) {
     var messageContent = messageInput.value.trim();
-    if(messageContent && stompClient) {
+    if (messageContent && stompClient) {
         var chatMessage = {
             sender: username,
             content: messageInput.value,
@@ -69,10 +80,14 @@ function sendMessage(event) {
 
 function onMessageReceived(payload) {
     var message = JSON.parse(payload.body);
+    displayMessage(message);
+}
+
+function displayMessage(message) {
 
     var messageElement = document.createElement('li');
 
-    if(message.type === 'JOIN') {
+    if (message.type === 'JOIN') {
         messageElement.classList.add('event-message');
         message.content = message.sender + ' joined!';
     } else if (message.type === 'LEAVE') {
@@ -114,5 +129,48 @@ function getAvatarColor(messageSender) {
     return colors[index];
 }
 
+function displayUser(user) {
+    var userElement = document.createElement('li');
+
+    userElement.classList.add('chat-message');
+
+    var avatarElement = document.createElement('i');
+    var avatarText = document.createTextNode(user.name[0]);
+    avatarElement.appendChild(avatarText);
+    avatarElement.style['background-color'] = getAvatarColor(user.name);
+
+    userElement.appendChild(avatarElement);
+
+    var usernameElement = document.createElement('span');
+    var usernameText = document.createTextNode(user.name);
+    usernameElement.appendChild(usernameText);
+    userElement.appendChild(usernameElement);
+
+    listArea.appendChild(userElement);
+    listArea.scrollTop = listArea.scrollHeight;
+}
+
+async function showOnline() {
+    usernamePage.classList.add('hidden');
+    listPage.classList.remove('hidden');
+    chatPage.classList.add('hidden');
+
+    listArea.innerHTML = "";
+
+    var response = await axios.get(DOMAIN + '/users');
+    for (var user of response.data) {
+        displayUser(user)
+    }
+
+}
+
+function backToChat() {
+    usernamePage.classList.add('hidden');
+    listPage.classList.add('hidden');
+    chatPage.classList.remove('hidden');
+}
+
 usernameForm.addEventListener('submit', connect, true)
 messageForm.addEventListener('submit', sendMessage, true)
+onlineButton.addEventListener('click', showOnline, true)
+backButton.addEventListener('click', backToChat, true)
